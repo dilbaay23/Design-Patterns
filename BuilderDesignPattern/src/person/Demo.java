ee2748f81f2e07983858c6818faea59ab4ff86f3
Moon <dilbaay@gmail.com>
2021-02-02 22:13:44 +0100

    test **.iml

---
+++ BuilderDesignPattern/src/person/Demo.java
@@ -0,0 +1,11 @@
+package person;
+
+/**
+ * Created By Moon
+ * 2/2/2021, Tue
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("zzz");
+    }
+}
---
+++ module/src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created By Moon
+ * 2/2/2021, Tue
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
